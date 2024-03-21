d55423a5bd9af89814aded5430bb5ea4df587ea6
GuFvr <gustavo.vfavero@gmail.com>
2024-03-20 21:42:19 -0300

    feat: Implement task addition functionality
    Added functionality to allow users to add tasks to the list. The Header component now includes text input for users to type their tasks. Upon pressing the add button, a new task is added to the list if the input is not empty and has a length of at least 5 characters. Implemented a UUID generation function to assign unique IDs to tasks.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,14 +5,23 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import logoImage from '../../assets/Logo.png'
 
-export function Header() {
+type HeaderProps = {
+  task: string;
+  onChangeText: (task: string) => void;
+  onPress: () => void;
+}
+
+export function Header({ task, onChangeText, onPress }: HeaderProps) {
   return <View style={styles.headerContainer}>
     <Image source={logoImage} />
     <View style={styles.form}>
       <TextInput style={styles.input}
         placeholder="Adicione uma nova tarefa"
-        placeholderTextColor={theme.colors.base.gray300} />
-      <TouchableOpacity style={styles.button}>
+        placeholderTextColor={theme.colors.base.gray300} 
+        value={task}
+        onChangeText={onChangeText}
+        />
+      <TouchableOpacity style={styles.button} onPress={onPress}>
         <MaterialCommunityIcons name="plus-circle-outline" size={22} color={theme.colors.base.gray100} />
       </TouchableOpacity>
     </View>
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,17 +1,29 @@
-import { FlatList, Text, View } from "react-native";
+import { Alert, FlatList, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Header } from "../components/Header";
 import { Task } from "../components/Task";
 import { useState } from "react";
 import { taskDTO } from "../dtos/TaskDTO";
 import { Empty } from "../components/Empty";
+import { uuid } from "../utils/uuid";
 
 export function HomeScreen() {
   const [tasks, setTasks] = useState<taskDTO[]>([])
+  const [newTask, setNewTask] = useState('')
+
+  function handleTaskAdd() {
+    if(newTask !== '' && newTask.length >= 5) {
+      setTasks((tasks => [... tasks, {id: uuid(), isCompleted: false, title: newTask.trim()}]))
+    }
+    else {
+      Alert.alert("Não é possível adicionar uma tarefa vazia ou menor que 5 digitos.")
+    }
+    setNewTask('')
+  }
 
   return (
     <View style={styles.container}>
-      <Header />
+      <Header task={newTask} onChangeText={setNewTask} onPress={handleTaskAdd} />
       <View style={styles.tasksContainer}>
         <View style={styles.info}>
           <View style={styles.row}>
